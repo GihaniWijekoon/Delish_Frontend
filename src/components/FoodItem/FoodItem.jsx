05a7a5c78c9353e2602d276008b65c301c2ca359
GihaniWijekoon <124669194+GihaniWijekoon@users.noreply.github.com>
2024-09-29 22:26:28 +0530

    feat: home page complited

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
-function FoodItem({id,name,price,description,image}) {
+const FoodItem = ({_id,name,price,description,image}) => {
+
+  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+
+  
+
   return (
     <div className='food-item'>
         <div className='food-item-img-container'>
             <img className='food-item-image' src={image} alt='' />
+            {!cartItems[_id]
+                ?<img className='add' onClick={()=>addToCart(_id)} src={assets.add_icon_white} alt='' />
+                :<div className='food-item-counter'>
+                  <img onClick={()=>removeFromCart(_id)} src={assets.remove_icon_red} alt=''/>
+                  <p>{cartItems[_id]}</p>
+                  <img onClick={()=>addToCart(_id)} src={assets.add_icon_green} alt='' />
+                </div>
+            }
         </div>
         <div className='food-item-info'>
             <div className='food-item-name-rating'>
